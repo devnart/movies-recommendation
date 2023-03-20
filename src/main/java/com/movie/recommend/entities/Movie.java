a49519510c1aa7d50bbef29f1ee1d9c37bcc3a42
devnart <hr.fb001@gmail.com>
2023-03-20 23:45:44 +0000

    movie model

--- src/main/java/com/movie/recommend/entities/Movie.java
+++ src/main/java/com/movie/recommend/entities/Movie.java
@@ -1,4 +1,60 @@
 package com.movie.recommend.entities;
 
 public class Movie {
+    private Long id;
+    private String title;
+    private String genre;
+    private int releaseYear;
+    private String director;
+
+    public Movie() {
+
+    }
+    public Movie(Long id, String title, String genre, int releaseYear, String director) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+        this.director = director;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
 }
